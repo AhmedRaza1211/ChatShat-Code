c2a275cb4be3bebaf8a3376d335dbdd84c3d753b
Ahmed Raza Attari <ahmedraza8325@gmail.com>
2021-03-28 00:30:15 +0500

    code

---
+++ app/src/main/java/com/example/chatshat_features/SettingsActivity.java
@@ -0,0 +1,14 @@
+package com.example.chatshat_features;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
